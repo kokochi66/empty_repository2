b898c3b51e20494ca01f2b476fdf086747ce7063
kokochi66 <kokochikochi@gmail.com>
2020-10-08 23:15:19 +0900

    ADD20201008

---
+++ codingsample/202010/20201008 [pro]/pro_MonthChel1008_3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class pro_MonthChel1008_3 {
+    public static int solution(int n, int[][] edges) {
+        for(int i=0;i<edges.length;i++){
+
+        }
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args){
+        int[][] a = {
+            {1,2},
+            {2,3},
+            {3,4}
+        };
+        int[][] b = {
+            {1,5},
+            {2,5},
+            {3,5},
+            {4,5}
+        };
+        System.out.println(solution(5,b));
+    }
+}
