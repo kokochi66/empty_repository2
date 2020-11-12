a64616fab7c6b7feb1565e14e074081cd39fd72e
kokochi66 <kokochikochi@gmail.com>
2020-11-12 23:44:33 +0900

    2020.11.12.study

--- CodeForce/100~/CF_101.java
+++ CodeForce/100~/CF_101.java
@@ -15,31 +15,16 @@ public class CF_101 {
     public static void main(String[] args) throws Exception {
         n = Integer.parseInt(Read.readLine());
         darr = new int[n][2];
-        doper = new int[n][2];
         used = new boolean[n];
-        counter = new int[7][n];
-        CS = new int[7];
+        oper = new int[n];
 
         for(int i=0;i<n;i++){
             tok = new StringTokenizer(Read.readLine());
             darr[i][0] = Integer.parseInt(tok.nextToken());
             darr[i][1] = Integer.parseInt(tok.nextToken());
-            counter[darr[i][0]][CS[darr[i][0]]] = i+1;
-            CS[darr[i][0]]++;
-            counter[darr[i][1]][CS[darr[i][1]]] = -1*(i+1);
-            CS[darr[i][1]]++;
         }
-        finish = false;
+        
 
-        for(int i=0;i<n;i++){
-            used[i] = true;
-            doper[0][0] = i;
-            doper[0][1] = 1;
-            stk(1,darr[i][1]);
-            doper[0][1] = -1;
-            stk(1,darr[i][0]);
-            used[i] = false;
-        }
         if(!finish) Write.write("No solution");
         Write.flush();
     }
---
+++ cppTraining.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+//#include <iostream>
+//#include <string>
+//#include <memory>
+//#include <algorithm>
+//#include <vector>
+//#include <queue>
+//#include <map>
