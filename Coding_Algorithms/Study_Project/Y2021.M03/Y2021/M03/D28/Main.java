40e23f96c56154ede8f484c339eba8ed65c4e4d3
kokochi66 <kokochikochi@gmail.com>
2021-03-30 11:48:58 +0900

    2021.03.30.Study

--- Coding_Algorithms/Study_Project/Y2021.M03/Y2021/M03/D28/Main.java
+++ Coding_Algorithms/Study_Project/Y2021.M03/Y2021/M03/D28/Main.java
@@ -25,7 +25,7 @@ public class Main {
 				} else {
 					map.put(arr[i], new Parci(arr[i], i));
 				}
-			}
+			}	// 입력된 값들을 정리하여 Map안에 넣음
 			
 			ArrayList<Parci> list = new ArrayList<Parci>();
 			ArrayList<Long> dp = new ArrayList<Long>();
@@ -35,13 +35,13 @@ public class Main {
 				int k = iter.next();
 				Parci p = map.get(k);
 				list.add(p);
-			}
+			}	// Map안에 넣은 값들을 꺼내서 리스트에 넣음
 			
 			Collections.sort(list);
 			dp.add(list.get(0).token * list.get(0).number.size());
 			for(int i=1;i<list.size();i++) {
 				dp.add(dp.get(i-1) + (list.get(i).token * list.get(i).number.size()));
-			}
+			}	// 리스트를 정렬하고, dp로 각 토큰수의 인원수만큼을 설정함.
 			
 			boolean[] win = new boolean[list.size()];
 			HashSet<Integer> winner = new HashSet<Integer>();
@@ -49,15 +49,13 @@ public class Main {
 			win[win.length-1] = true;
 			winner.add((int) list.get(list.size()-1).token);
 			printsize += list.get(list.size()-1).number.size();
-			
-			
 			for(int i=list.size()-2;i>=0;i--) {
-				if(dp.get(i) >= list.get(i+1).token) {
+				if(dp.get(i) >= list.get(i+1).token && win[i+1]) {
 					win[i] = true;
 					winner.add((int) list.get(i).token);
 					printsize += list.get(i).number.size();
 				}
-			}
+			}	// dp값이 다음값보다 크다면 그 다음값을 각각확인, 그 다음값이 우승값이라면 현재 dp값도 우승으로 판정
 			
 			wr.write(printsize+" ");
 			wr.newLine();
