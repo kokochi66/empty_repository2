df1c9a8f99d8a5a3830b56a5d6cc4e9281409daa
kokochi66 <kokochikochi@gmail.com>
2021-04-10 22:22:08 +0900

    2021.04.10.Study

--- Coding_Algorithms/Study_Project/src/test.java
+++ Coding_Algorithms/Study_Project/src/test.java
@@ -5,6 +5,11 @@ public class test {
 	static BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer tok;
 	public static void main(String[] args) throws Exception {
-		
+	}
+	
+	public static int Pib(int n) throws Exception {
+		if(n ==  0) return 1;
+		else if(n == 1) return 1;
+		return Pib(n-1) + Pib(n-2);
 	}
 }
