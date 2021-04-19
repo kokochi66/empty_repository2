849664c3debcd0fe31021a124aba8f5c4b21b864
kokochi66 <kokochikochi@gmail.com>
2021-04-20 08:07:39 +0900

    2021.04.20.Study

--- Coding_Algorithms/Study_Project/Y2021.M04/Y2021/M04/D19/Main.java
+++ Coding_Algorithms/Study_Project/Y2021.M04/Y2021/M04/D19/Main.java
@@ -8,83 +8,44 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		solution();
 	}
+	
+	static boolean[] chk;
+	static ArrayList<Integer[]>[] nd;
+	public static void init() {
+		Arrays.fill(chk, false);
+	}
+	
+	public static int dfs(int n, int len) {
+		int max = len;
+		chk[n] = true;
+		for(int i=0;i<nd[n].size();i++) {
+			Integer[] c = nd[n].get(i);
+			if(!chk[c[0]]) max = Math.max(max, dfs(c[0], len + c[1]));
+		}
+		return max;
+	}
 	public static void solution() throws Exception {
 		int n = Integer.parseInt(rd.readLine());
-		int[][] dp = new int[n+1][n+1];
-		int INF = 1000000000;
-		int max = 0;
+		nd = new ArrayList[n+1];
+		chk = new boolean[n+1];
+		for(int i=0;i<=n;i++) nd[i] = new ArrayList<Integer[]>();
 		for(int i=0;i<n-1;i++) {
 			tok = new StringTokenizer(rd.readLine());
 			int a = Integer.parseInt(tok.nextToken());
 			int b = Integer.parseInt(tok.nextToken());
 			int c = Integer.parseInt(tok.nextToken());
-			dp[a][b] = c;
-			dp[b][a] = c;
-			if(max < c) max = c;
+			nd[a].add(new Integer[] {b,c});
+			nd[b].add(new Integer[] {a,c});
+		} // 입력부
+		int[] max = new int[nd[1].size()];
+		for(int i=0;i<nd[1].size();i++) {
+			init();
+			chk[1] = true;
+			Integer[] c = nd[1].get(i);
+			max[i] = dfs(c[0], c[1]);
 		}
-		// 입력부
-		
-		Queue<Integer> q = new LinkedList<>();
-		boolean[] chk = new boolean[n+1];
-		q.add(1);
-		int Leaf =0;
-		while(!q.isEmpty()) {
-			Leaf = q.poll();
-			chk[Leaf] = true;
-			for(int i=1;i<=n;i++) {
-				if(!chk[i] && dp[Leaf][i] > 0) {
-					q.add(i);
-					break;
-				}
-			}
-		}
-		
-		if(n > 1) {
-			Arrays.fill(chk, false);
-			ArrayList<Integer> ts = new ArrayList<>();
-			int root  = 0;
-			for(int i=1;i<=n;i++) {
-				if(!chk[i] && dp[Leaf][i] > 0) {
-					root = i;
-					break;
-				}
-			}
-			chk[Leaf] = true;
-			chk[root] = true;
-			ts.add(Leaf);
-			ts.add(root);
-			
-			Queue<Integer[]> qs = new LinkedList<>();
-			for(int i=1;i<=n;i++) {
-				if(!chk[i] && dp[root][i] > 0) {
-					qs.add(new Integer[] {i, root});
-				}
-			}
-			
-			while(!qs.isEmpty()) {
-				int cn = qs.peek()[0];
-				int cr = qs.poll()[1];
-				chk[cn] = true;
-				for(int i=0;i<ts.size();i++) {
-					int cw = ts.get(i);
-					int dist = dp[cn][cr] + dp[cr][cw];
-					if(dist > max) max = dist;
-					dp[cn][cw] = dist;
-					dp[cw][cn] = dist;
-				}
-				for(int i=1;i<=n;i++) {
-					if(!chk[i] && dp[cn][i] > 0) {
-						qs.add(new Integer[] {i, cn});
-					}
-				}
-				ts.add(cn);
-			}
-		}
-		
-//		for(int i=0;i<=n;i++) {
-//			System.out.println(Arrays.toString(dp[i]));
-//		}
-		System.out.println(max);
-		
+		System.out.println(Arrays.toString(max));
+		Arrays.sort(max);
+		System.out.println(max.length >=2 ? max[0]+max[1] : max[0]);
 	}
 }
