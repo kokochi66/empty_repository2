6ca40f2bece715c6210e3bdfe72449bd5262705d
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2021-11-19 17:49:40 +0900

    프로그래머스 문제풀이

--- CodeForce/java/src/Main/Main.java
+++ CodeForce/java/src/Main/Main.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -18,47 +19,184 @@ public class Main {
     static boolean[] used, check;
     static int h, w, n, m;
     public static void main(String[] args) throws Exception {
-//        solution(25, new int[]{2,14,11,21,17}, 2); // 4
-//        solution(25, new int[]{6, 11, 14, 17, 21}, 2); // 5
-//        solution(25, new int[]{10}, 1); // 25
-//        solution(25, new int[]{10,10}, 2); // 25
-//        solution(16, new int[]{4,8,11}, 2); // 8
-//        solution(25, new int[]{2, 6, 10, 14, 17, 22, 24}, 4); // 4
-//        solution(28, new int[]{2,6,10,14,17,22,27}, 4); // 6
-//        solution(32, new int[]{4, 8, 11, 16, 23, 29}, 3); // 7
+//        String[] enroll = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
+//        String[] referral = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
+//        String[] seller = {"young", "john", "tod", "emily", "mary"};
+//        int[] amount = {12, 4, 2, 5, 10};
+//        solution(enroll, referral, seller, amount);
+        int[][] key = {
+                {0,0,0},
+                {1,0,0},
+                {0,1,1}
+        };
+
+        int[][] lock = {
+                {1,1,1},
+                {1,1,0},
+                {1,0,1}
+        };
+
+        System.out.println(solution(key, lock));
     }
 
-    public static int solution(int distance, int[] rocks, int n) {
-        Arrays.sort(rocks);
-        int[] rockArr = new int[rocks.length+1];
-        for(int i=0;i< rocks.length;i++) rockArr[i] = rocks[i];
-        rockArr[rockArr.length - 1] = distance;
-        int res  = binarySearch(0, distance * 2 , rockArr, n);
-        System.out.println(res);
-        return 0;
-    }
 
-    public static int binarySearch(int left, int right, int[] rocks, int n) {
-        int mid = (left + right) / 2;
+    public static boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
+        int n = key.length;
+        int m = lock.length;
 
-        int res = checkRock(mid, rocks, n);
-//        System.out.println("MID :: " + mid+" "+left+" "+right+" "+res);
-        if(res == 1 && right - left <= 1) return mid;
-        return res == 1 ? binarySearch(mid, right, rocks, n) : binarySearch(left, mid, rocks, n);
-    }
-
-    public static int checkRock(int target, int[] rocks, int n) {
-        int curr = 0;
-        for(int i=0;i<rocks.length;i++) {
-            if(rocks[i] - curr < target) {
-//                System.out.println("count :: " + curr +" "+rocks[i]+" "+n+" "+target);
-                n--;
-            } else {
-                curr = rocks[i];
+        int minX=m, minY=m, maxX=0, maxY=0;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<m;j++) {
+                if(lock[i][j] == 0) {
+                    if(i < minY) minY = i;
+                    if(j < minX) minX = j;
+                    if(i > maxY) maxY = i;
+                    if(j > maxX) maxX = j;
+                }
             }
-            if(n < 0) return -1;
         }
-        return 1;
+
+        int[][] pan = new int[maxY-minY+1][maxX-minX+1];
+        for(int i=minY;i<=maxY;i++) {
+            for(int j=minX;j<=maxX;j++) {
+                pan[i-minY][j-minX] = lock[i][j];
+            }
+        }
+        for(int i=0;i<pan.length;i++) System.out.println(Arrays.toString(pan[i]));
+
+        // 4가지 회전하여 일부가 맞는 부분이 있으면 통과 (패널을 돌면서)
+
+        int[][] rot90 = new int[n][n];
+        int[][] rot180 = new int[n][n];
+        int[][] rot270 = new int[n][n];
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                rot90[i][j] = key[n-j-1][i];
+                rot180[i][j] = key[n-i-1][n-j-1];
+                rot270[i][j] = key[j][n-i-1];
+            }
+        }
+        // 90도 회전 -> n-j-1, i
+        // 180도 회전 -> n-i-1, n-j-1
+        // 270도 회전 -> j, n-i-1
+
+        System.out.println("==== key ===");
+        for(int i=0;i<n;i++) System.out.println(Arrays.toString(key[i]));
+        System.out.println("==== //key ===");
+        System.out.println("==== key ===");
+        for(int i=0;i<n;i++) System.out.println(Arrays.toString(rot90[i]));
+        System.out.println("==== //key ===");
+        System.out.println("==== key ===");
+        for(int i=0;i<n;i++) System.out.println(Arrays.toString(rot180[i]));
+        System.out.println("==== //key ===");
+        System.out.println("==== key ===");
+        for(int i=0;i<n;i++) System.out.println(Arrays.toString(rot270[i]));
+        System.out.println("==== //key ===");
+
+        int panX = pan[0].length;
+        int panY = pan.length;
+        for(int i=0;i<n - panY;i++) {
+            for(int j=0;j<n - panX;j++) {
+                boolean check0 = false;
+                boolean check90 = false;
+                boolean check180 = false;
+                boolean check270 = false;
+
+                for(int k=0;k<panY;k++) {
+                    for(int l=0;l<panX;l++) {
+                        if(pan[k][l] == key[i+k][j+l]) check0 = true;
+                        if(pan[k][l] == rot90[i+k][j+l]) check90 = true;
+                        if(pan[k][l] == rot180[i+k][j+l]) check180 = true;
+                        if(pan[k][l] == rot270[i+k][j+l]) check270 = true;
+                        if(check0 && check90 && check180 && check270) break;
+                    }
+                    if(check0 && check90 && check180 && check270) break;
+                }
+                if(!(check0 && check90 && check180 && check270)) return true;
+            }
+        }
+        return false;
+    }
+    public static int[] solution2(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int n = enroll.length;
+
+        HashMap<String, Member> memMap = new HashMap<>();
+        memMap.put("-", new Member("-", null, 0));
+        for(int i=0;i<n;i++) {
+            Member mem = new Member(enroll[i], memMap.get(referral[i]), 0);
+            memMap.put(enroll[i], mem);
+        }
+        // 초기화
+
+        int m = seller.length;
+        for(int i=0;i<m;i++) {
+            Member mem = memMap.get(seller[i]);
+            mem.perMoney = amount[i] * 100;
+            while(mem.parent != null) {
+                int mon = mem.perMoney;
+                int pmon = mon/10;
+                mon -= pmon;
+//                if(mem.name.equals("mary")) System.out.println("MARY :: " + mon +" " + pmon);
+                mem.money += mon;
+                mem.parent.perMoney = pmon;
+                mem = mem.parent;
+            }
+        }
+        int[] answer = new int[n];
+        for(int i=0;i<n;i++) {
+            Member mem = memMap.get(enroll[i]);
+            answer[i] = mem.money;
+ //            System.out.println(mem  +" " + (mem.parent != null ? mem.parent.name : ""));
+        }
+
+
+        return answer;
     }
 
+    static class Member {
+        String name;
+        Member parent;
+        int money;
+        int perMoney;
+
+        public Member(String name, Member parent, int money) {
+            this.name = name;
+            this.parent = parent;
+            this.money = money;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Member getParent() {
+            return parent;
+        }
+
+        public void setParent(Member parent) {
+            this.parent = parent;
+        }
+
+        public int getMoney() {
+            return money;
+        }
+
+        public void setMoney(int money) {
+            this.money = money;
+        }
+
+        @Override
+        public String toString() {
+            return "Member{" +
+                    "name='" + name + '\'' +
+                    ", money=" + money +
+                    '}';
+        }
+    }
 }
