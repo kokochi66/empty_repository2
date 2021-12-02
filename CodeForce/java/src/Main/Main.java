f622fda64021b70d27f1a589528ecc64e6c12d32
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2021-12-02 17:48:17 +0900

    백준 문제풀이

--- CodeForce/java/src/Main/Main.java
+++ CodeForce/java/src/Main/Main.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -16,6 +17,39 @@ public class Main {
     }
 
     public static void solution() throws Exception {
+        tok = new StringTokenizer(rd.readLine());
+        int n = Integer.parseInt(tok.nextToken());
+        int m = Integer.parseInt(tok.nextToken());
+        int[][] arr = new int[n][m];
+        for(int i=0;i<n;i++) {
+            String str = rd.readLine();
+            for(int j=0;j<m;j++) {
+                arr[i][j] = Integer.parseInt(Character.toString((str.charAt(j))));
+            }
+        }
+        for(int i=0;i<n;i++) System.out.println(Arrays.toString(arr[i]));
+
+        long max = 0;
+        for(int i=0;i<n;i++) {
+            for(int l=0;l<n;l++) {
+                for(int r=0;r<m;r++) {
+                    int x = i;
+                    int y = 0;
+                    StringBuilder res = new StringBuilder("");
+                    res.append(arr[x][y]);
+                    while(x < n && y < m) {
+                        x += l;
+                        y += r;
+                    }
+                    arr[x+l][y+r]++;
+
+
+                }
+            }
+        }
+    }
+
+    public static void solution2() throws Exception {
         int TT = Integer.parseInt(rd.readLine());
         for(int TS=0;TS<TT;TS++) {
             StringBuilder s = new StringBuilder(rd.readLine());
@@ -23,7 +57,7 @@ public class Main {
             boolean check = false;
 
             while(idx < s.length()) {
-                System.out.println("TEST :: " + idx + "  "+s.charAt(idx));
+//                System.out.println("TEST :: " + idx + "  "+s.charAt(idx));
                 if(s.charAt(idx) == '1') {
                     // 패턴 1
                     idx++;
@@ -43,7 +77,8 @@ public class Main {
                     }    // 범위체크
                     int idxC = idx;
                     while(idx < s.length() && s.charAt(idx) == '1') idx++;     // 연속된 1 체크
-                    if(idx - idxC > 0 && idx + 1 < s.length() && s.charAt(idx+1) == '0') idx--;
+                    if(idx - idxC > 1 && idx + 1 < s.length() && s.charAt(idx+1) == '0') idx--;
+
                 } else {
                     // 패턴 2
                     idx++;
