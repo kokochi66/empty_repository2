8730f9fa018f0e85683df1ade2eb795aab217988
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2021-12-02 17:24:15 +0900

    프로그래머스 문제풀이

--- CodeForce/java/src/Main/Main.java
+++ CodeForce/java/src/Main/Main.java
@@ -1,9 +1,11 @@
 package src.Main;
 
 
-import java.io.*;
-import java.math.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
@@ -16,8 +18,47 @@ public class Main {
     public static void solution() throws Exception {
         int TT = Integer.parseInt(rd.readLine());
         for(int TS=0;TS<TT;TS++) {
-            tok = new StringTokenizer(rd.readLine());
+            StringBuilder s = new StringBuilder(rd.readLine());
+            int idx = 0;
+            boolean check = false;
 
+            while(idx < s.length()) {
+                System.out.println("TEST :: " + idx + "  "+s.charAt(idx));
+                if(s.charAt(idx) == '1') {
+                    // 패턴 1
+                    idx++;
+                    if(idx >= s.length() || s.charAt(idx) == '1') {
+                        check=true;
+                        break;
+                    }    // 0 체크
+                    idx++;
+                    if(idx >= s.length() || s.charAt(idx) == '1') {
+                        check=true;
+                        break;
+                    }    // 0 체크
+                    while(idx < s.length() && s.charAt(idx) == '0') idx++;     // 연속된 0 체크
+                    if(idx >= s.length()) {
+                        check=true;
+                        break;
+                    }    // 범위체크
+                    int idxC = idx;
+                    while(idx < s.length() && s.charAt(idx) == '1') idx++;     // 연속된 1 체크
+                    if(idx - idxC > 0 && idx + 1 < s.length() && s.charAt(idx+1) == '0') idx--;
+                } else {
+                    // 패턴 2
+                    idx++;
+                    if(idx >= s.length() || s.charAt(idx) == '0') {
+                        check=true;
+                        break;
+                    }    // 1 체크
+                    idx++;
+                }
+
+            }
+            if(check) wr.write("NO");
+            else wr.write("YES");
+            wr.newLine();
         }
+        wr.flush();
     }
 }
--- CodeForce/java/src/Main/수학/진법변환.java
+++ CodeForce/java/src/Main/수학/진법변환.java
@@ -2,15 +2,32 @@ package src.Main.수학;
 
 public class 진법변환 {
 
-    public static int 진법변환3(int n) {
-        int oper = 1;
-        int res = 0;
-        while(n > 0) {
-            res += (n%3) * oper;
-            n/=3;
-            oper *= 10;
-        }
-
-        return res;
+    public static void main(String[] args) {
+        System.out.println(parse16(78));
+//        System.out.println((char)('A'+3));
     }
+
+
+    public static StringBuilder parse(long n) {
+        long oper = 1;
+        long ver = 2;
+        StringBuilder res = new StringBuilder("");
+        while(n > 0) {
+            res.append((n%ver));
+            n/=ver;
+        }
+        return res.reverse();
+    }   // 2진법 변환
+
+    public static StringBuilder parse16(long n) {
+        long ver = 16;
+        StringBuilder res = new StringBuilder("");
+        while(n > 0) {
+            int op = (int)(n%ver);
+            if(n%ver < 10) res.append(op);
+            else  res.append((char)('A'+(op-10)));
+            n/=ver;
+        }
+        return res.reverse();
+    }   // 2진법 변환
 }
