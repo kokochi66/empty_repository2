98a872f6c2c8e902b8313b0112af8117b7f91f0d
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2021-12-07 17:17:29 +0900

    백준 문제풀이

--- CodeForce/java/src/Main/Main.java
+++ CodeForce/java/src/Main/Main.java
@@ -5,8 +5,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
@@ -17,83 +16,110 @@ public class Main {
     }
 
     public static void solution() throws Exception {
-        tok = new StringTokenizer(rd.readLine());
-        int n = Integer.parseInt(tok.nextToken());
-        int m = Integer.parseInt(tok.nextToken());
-        int[][] arr = new int[n][m];
-        for(int i=0;i<n;i++) {
-            String str = rd.readLine();
-            for(int j=0;j<m;j++) {
-                arr[i][j] = Integer.parseInt(Character.toString((str.charAt(j))));
-            }
-        }
-        for(int i=0;i<n;i++) System.out.println(Arrays.toString(arr[i]));
-
-        long max = 0;
-        for(int i=0;i<n;i++) {
-            for(int l=0;l<n;l++) {
-                for(int r=0;r<m;r++) {
-                    int x = i;
-                    int y = 0;
-                    StringBuilder res = new StringBuilder("");
-                    res.append(arr[x][y]);
-                    while(x < n && y < m) {
-                        x += l;
-                        y += r;
-                    }
-                    arr[x+l][y+r]++;
-
-
-                }
-            }
-        }
-    }
-
-    public static void solution2() throws Exception {
         int TT = Integer.parseInt(rd.readLine());
         for(int TS=0;TS<TT;TS++) {
-            StringBuilder s = new StringBuilder(rd.readLine());
-            int idx = 0;
-            boolean check = false;
+            tok = new StringTokenizer(rd.readLine());
+            int n = Integer.parseInt(tok.nextToken());
+            int m = Integer.parseInt(tok.nextToken());
 
-            while(idx < s.length()) {
-//                System.out.println("TEST :: " + idx + "  "+s.charAt(idx));
-                if(s.charAt(idx) == '1') {
-                    // 패턴 1
-                    idx++;
-                    if(idx >= s.length() || s.charAt(idx) == '1') {
-                        check=true;
-                        break;
-                    }    // 0 체크
-                    idx++;
-                    if(idx >= s.length() || s.charAt(idx) == '1') {
-                        check=true;
-                        break;
-                    }    // 0 체크
-                    while(idx < s.length() && s.charAt(idx) == '0') idx++;     // 연속된 0 체크
-                    if(idx >= s.length()) {
-                        check=true;
-                        break;
-                    }    // 범위체크
-                    int idxC = idx;
-                    while(idx < s.length() && s.charAt(idx) == '1') idx++;     // 연속된 1 체크
-                    if(idx - idxC > 1 && idx + 1 < s.length() && s.charAt(idx+1) == '0') idx--;
-
-                } else {
-                    // 패턴 2
-                    idx++;
-                    if(idx >= s.length() || s.charAt(idx) == '0') {
-                        check=true;
-                        break;
-                    }    // 1 체크
-                    idx++;
-                }
-
+            tok = new StringTokenizer(rd.readLine());
+            Node[] arr = new Node[n+1];
+            long[] orinTime = new long[n+1];
+            for(int i=0;i<=n;i++) arr[i] = new Node(i);
+            for(int i=1;i<=n;i++) {
+                arr[i].time = Long.parseLong(tok.nextToken());
+                orinTime[i] = arr[i].time;
             }
-            if(check) wr.write("NO");
-            else wr.write("YES");
+
+            for(int i=0;i<m;i++) {
+                tok = new StringTokenizer(rd.readLine());
+                int idx = Integer.parseInt(tok.nextToken());
+                int next = Integer.parseInt(tok.nextToken());
+                arr[idx].next.add(arr[next]);
+                arr[next].need.add(arr[idx]);
+            }
+
+            int target = Integer.parseInt(rd.readLine());
+            boolean[] setCheck = new boolean[n+1];
+
+            HashSet<Integer> indexSet = new HashSet<>();
+            Queue<Node> nq = new LinkedList<>();
+            nq.add(arr[target]);
+            while(!nq.isEmpty()) {
+                Node node = nq.poll();
+                if(setCheck[node.index]) continue;
+                setCheck[node.index] = true;
+                indexSet.add(node.index);
+                for(int i=0;i<node.need.size();i++) {
+                    if(!setCheck[node.need.get(i).index]) {
+                        nq.add(node.need.get(i));
+                    }
+                }
+            }
+
+            long max = 0;
+            Queue<Node> q = new LinkedList<>();
+            for(int i=1;i<=n;i++) {
+                if(arr[i].need.size() == 0 && indexSet.contains(arr[i].index)) {
+                    Node node = new Node(arr[i].index);
+                    node.time = arr[i].time;
+                    q.add(node);
+                }
+            }
+            while(!q.isEmpty()) {
+                Node node = q.poll();
+                List<Node> next = arr[node.index].next;
+                for(int i=0;i<next.size();i++) {
+                    Node cn = next.get(i);
+                    if(indexSet.contains(cn.index)) {
+                        List<Node> need = cn.need;
+                        boolean check = false;
+                        for(int j=0;j<need.size();j++) {
+                            Node ncn = need.get(j);
+                            if(!ncn.cons) {
+                                check = true;
+                                break;
+                            }
+                        }
+                        if(!check) {
+                            // 조건이 모두 갖췄다면 큐에 추가
+                            Node nn = new Node(cn.index);
+                            nn.time = node.time + arr[cn.index].time;
+                        }
+                    }
+                }
+                wr.write(node.toString());
+                wr.newLine();
+            }
+
+
+            wr.write(arr[target].time+"");
             wr.newLine();
         }
         wr.flush();
     }
+
+    static class Node {
+        int index;
+        long time;
+        boolean cons;
+        List<Node> need;
+        List<Node> next;
+        public Node(int index) {
+            this.index = index;
+            this.cons = false;
+            need = new ArrayList<>();
+            next = new ArrayList<>();
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "index=" + index +
+                    ", time=" + time +
+                    ", cons=" + cons +
+                    '}';
+        }
+    }
 }
+
