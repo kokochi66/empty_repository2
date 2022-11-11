8d7b88174b87db3adaa52eeb1d6dfe5325ee7a3a
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2022-11-11 23:04:40 +0900

    코테풀이

---
+++ CodeForce/java/src/Y2022/M11/main1111.java
@@ -0,0 +1,31 @@
+package src.Y2022.M11;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class main1111 {
+    static BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer tok;
+
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
+
+    public static void solution() throws Exception {
+        long k = Long.parseLong(rd.readLine());
+
+        // 0
+        // 01
+        // 0110
+        // 01101001
+        // 0110100101101001
+
+
+    }
+
+}
